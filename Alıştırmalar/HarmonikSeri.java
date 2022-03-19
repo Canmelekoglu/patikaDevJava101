8b3ad05dcc36ad72f700329d3f7228a9ea18afed
Izzet Can <icanmelekoglu@gmail.com>
2022-03-19 13:08:48 +0300

    Create HarmonikSeri.java
    https://app.patika.dev/moduller/java101/www.patika.dev

---
+++ Alıştırmalar/HarmonikSeri.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class HarmonikSeri {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        System.out.print("HArmonik seri için bir sayı giriniz: ");
+        int n = inp.nextInt();
+        double result = 0;
+
+        for (double i=1 ; i<=n ; i++) {
+            result += (1/i);
+        }
+        System.out.println(n+ " sayısının harmonik seri toplamı: "+result);
+    }
+}
